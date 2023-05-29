53034c155bc525ad2ca1e2ec660b77389906a715
phantomswordfish09 <dylannasution@gmail.com>
2023-05-29 14:02:35 -0400

    basic routes CRUD routes for products api works

--- routes/products.router.ts
+++ routes/products.router.ts
@@ -7,6 +7,7 @@ export const productsRouter = express.Router();
 
 productsRouter.use(express.json());
 
+// Get all products
 productsRouter.get("/", async (_req: Request, res: Response) => {
     try {
         if (collections.products) {
@@ -19,8 +20,11 @@ productsRouter.get("/", async (_req: Request, res: Response) => {
     }
 });
 
+
+// Get product with id
 productsRouter.get("/:id", async (req: Request, res: Response) => {
     const id = req?.params?.id;
+    console.log(id)
 
     try {
 
@@ -35,5 +39,72 @@ productsRouter.get("/:id", async (req: Request, res: Response) => {
         }
     } catch (error) {
         res.status(404).send(`Unable to find matching document with id: ${req.params.id}`);
+    }
+});
+
+
+// Creates new product
+productsRouter.post("/", async (req: Request, res: Response) => {
+    try {
+        const newProduct = req.body as Product;
+        console.log(req.body)
+        if (collections.products) {
+            const result = await collections.products.insertOne(newProduct);
+
+            result
+            ? res.status(201).send(`Successfully created a new Product with id ${result.insertedId}`)
+            : res.status(500).send("Failed to create a new Product.");
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error);
+    }
+});
+
+
+// Updates existing product
+productsRouter.put("/:id", async (req: Request, res: Response) => {
+    const id = req?.params?.id;
+    console.log(req.body)
+
+    try {
+        const updatedProduct: Product = req.body as Product;
+        const query = { _id: new ObjectId(id) };
+        if (collections.products) {
+            const result = await collections.products.updateOne(query, { $set: updatedProduct });
+
+            result
+            ? res.status(200).send(`Successfully updated Product with id ${id}`)
+            : res.status(304).send(`Product with id: ${id} not updated`);
+        }
+
+        
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error);
+    }
+});
+
+
+// Deletes product with id
+productsRouter.delete("/:id", async (req: Request, res: Response) => {
+    const id = req?.params?.id;
+
+    try {
+        const query = { _id: new ObjectId(id) };
+        if (collections.products) {
+            const result = await collections.products.deleteOne(query);
+
+            if (result && result.deletedCount) {
+                res.status(202).send(`Successfully removed Product with id ${id}`);
+            } else if (!result) {
+                res.status(400).send(`Failed to remove Product with id ${id}`);
+            } else if (!result.deletedCount) {
+                res.status(404).send(`Product with id ${id} does not exist`);
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error);
     }
 });
