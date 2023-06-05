707af94e0747d7edecb4c4afd424f01962e0d36f
phantomswordfish09 <dylannasution@gmail.com>
2023-06-05 15:14:23 -0400

    cors works

--- server.ts
+++ server.ts
@@ -1,13 +1,23 @@
 import express, { Express } from "express";
 import { connectToDatabase } from "./services/database.service"
 import { productsRouter } from "./routes/products.router";
+import cors from "cors";
 
 const app: Express = express();
 
 const port = 3000;
+const allowedOrigins = ['http://localhost:4200'];
 
 connectToDatabase()
     .then(() => {
+        const options: cors.CorsOptions = {
+            origin: allowedOrigins
+        };
+
+        // Then pass these options to cors:
+        app.use(cors(options));
+        app.use(express.json());
+
         app.use("/products", productsRouter);
 
         app.listen(port, () => {
